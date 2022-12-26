4555ba029986ae80adc09f272c1fc30dc32bf42f
hajinasiri@appcosolution.com <q1w2e3r4t5>
2022-12-26 12:12:26 +0330

    add toggle-button storybook

---
+++ src/stories/toggle-button/toggle-button.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {ToggleButton} from '../../elements/toggle-button/toggle-button'
+
+export default {
+  title: 'PB Kit/Atoms/ToggleButton',
+  component: ToggleButton,
+  argTypes: {},
+} as ComponentMeta<typeof ToggleButton>;
+
+const Template: ComponentStory<typeof ToggleButton> = (args: any) => <ToggleButton {...args} />;
+
+export const Color = Template.bind({});
+Color.args = {
+  children: 'Toggle Button',
+  value: 'Toggle Button',
+};
